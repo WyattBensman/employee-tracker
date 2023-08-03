336a18c35b25cab4cb5f90e08e122b45c293e33e
WyattBensman <wyattbensman5@gmail.com>
2023-08-03 15:17:00 -0400

    Made syntax corrections to the Schema

--- schema.sql
+++ schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role(
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -24,7 +24,10 @@ CREATE TABLE employee(
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
   ON DELETE SET NULL
 );
